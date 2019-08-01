2e46848cf1b1580d3ea8f0d521901ad044f34dd5
ashwinirkamble <ashwinik695@gmail.com>
2019-08-01 23:05:00 +0530

    git

--- MyPro/src/app/app.component.ts
+++ MyPro/src/app/app.component.ts
@@ -6,15 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyPro';
-  text = 'Marvellous Infosystems';
-  public fun(): void
-  {
-    this.text = 'MARVELLOUS INFOSYSTEMS';
-  }
 
-  public fun1(): void
-  {
-    this.text = 'marvellous infosystems';
-  }
 }
